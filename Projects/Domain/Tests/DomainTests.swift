c7caf65c4b9f02ca93cfd74e37ec9d4538805855
YoonSuLee <ysleedev01@gmail.com>
2023-11-13 18:31:28 +0900

    Feat(App): DI 주입 및 AppCoordinator 생성

--- Projects/Domain/Tests/DomainTests.swift
+++
@@ -1,8 +0,0 @@
-//
-//  DomainTests.swift
-//  ProjectDescriptionHelpers
-//
-//  Created by 이윤수 on 11/7/23.
-//
-
-import Foundation
--- Projects/Main/Sources/View/MainView.swift
+++
@@ -1,8 +0,0 @@
-//
-//  MainView.swift
-//  ProjectDescriptionHelpers
-//
-//  Created by 이윤수 on 11/13/23.
-//
-
-import Foundation
